35246be7c5d6e5cfc166235c1adc4b09f896699c
Joey Wolter <sulfurshade@gmail.com>
2017-09-08 10:34:26 -0400

    new server

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ const blogPostsRouter = require('./blogPostsRouter');
 const app = express();
 app.use(morgan('common'));
 app.use('/blog-posts', blogPostsRouter);
-app.listen(process.env.PORT || 8080, () => {
-  console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
-});
+// app.listen(process.env.PORT || 8080, () => {
+//   console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
+// });
 
 let server;
 function runServer() {
--- test/test-server.js
+++ test/test-server.js
@@ -79,7 +79,7 @@ it('should delete posts on DELETE', function() {
 	return chai.request(app)
 		.get('/blog-posts')
 		.then(function(res) {
-			return chai-request(app)
+			return chai.request(app)
 				.delete('/blog-posts/$(res.body[0].id')
 				.then(function(res) {
 					res.should.have.status(204);
